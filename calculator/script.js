27dfdbeb6b61fc134d9f6b27b4fc53dd49cd58e8
Francisco Oscar Ballerio <fballerio@ciber.mil>
2025-06-27 12:28:48 -0300

    some logic

--- calculator/script.js
+++ calculator/script.js
@@ -1,34 +1,84 @@
 "use strict";
 
 let num1 = null;
-let num2 = null;
 let operator = null;
+let num2 = null;
+const display = document.getElementById("display");
 
 function numberClick(number) {
-    updateDisplay(number);
+    if (num1 === null && num2 === null) {
+        if (number === "0") {
+            // Do nothing if the first number is 0 and no operator is selected
+            return;
+        } else if (number === ".") {
+            // If the first number is not set, start with 0.
+            num1 = "0.";
+            updateDisplay(num1);
+            return;
+        }
+    }
     if (operator === null) {
         if (num1 === null) {
             num1 = number;
         } else {
-            num1 += number;
+            if (num1.length >= 8) {
+                // Limit the length of the number to 10 digits
+                return;
+            } else {
+                num1 += number;
+            }
         }
+        updateDisplay(num1);
+        return;
     } else {
         if (num2 === null) {
             num2 = number;
         } else {
-            num2 += number;
+            if (num2.length >= 8) {
+                // Limit the length of the number to 10 digits
+                return;
+            } else {
+                num2 += number;
+            }
         }
+        updateDisplay(num2);
+        return;
     }
 }
 
-function operatorClick(operator) {}
+function clearDisplay() {
+    display.innerText = "0";
+    num1 = null;
+    num2 = null;
+    operator = null;
+}
 
-function clearDisplay() {}
+function operatorClick(operator_clicked) {
+    if (num1 === null) {
+        return;
+    } 
+    if (num2 != null) {
+        res = calculate()
+        
+        display.innerText = res
+        num1 = res
+        num2 = null
+        operator = operator_clicked
+        return;
+    } else {
+        operator = operator_clicked
+        display.innerText = operator
+        return;
+    }
+}
 
-function calculate() {}
+
+function calculate() {
+    
+}
 
 function updateDisplay(value) {
-    const display = document.getElementById("display");
     display.innerText = value;
 }
 
+function toggleSign() {}
