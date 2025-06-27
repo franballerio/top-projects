1fa23479ecc8e606206fa82b174fe0755ffbcc69
franballerio <fballerio@dc.uba.ar>
2025-06-26 23:52:03 -0300

    Front + some actions

--- calculator/script.js
+++ calculator/script.js
@@ -3,32 +3,68 @@
 let num1 = null;
 let num2 = null;
 let operator = null;
+const display = document.getElementById("display");
 
 function numberClick(number) {
-    updateDisplay(number);
+
+
+
+    if (num1 === null && num2 === null) {
+        if (number === "0") {
+            // Do nothing if the first number is 0 and no operator is selected
+            return;
+        } else if (number === ".") {
+            // If the first number is not set, start with 0.
+            num1 = "0.";
+            updateDisplay(num1);
+            return;
+        }
+    }
+
     if (operator === null) {
         if (num1 === null) {
             num1 = number;
         } else {
-            num1 += number;
+            if (num1.length >= 8) {
+                // Limit the length of the number to 10 digits
+                return;
+            } else {
+                num1 += number;
+            }
         }
+        updateDisplay(num1);
+        return;
     } else {
         if (num2 === null) {
             num2 = number;
         } else {
-            num2 += number;
+            if (num2.length >= 8) {
+                // Limit the length of the number to 10 digits
+                return;
+            } else {
+                num2 += number;
+            }
         }
     }
 }
 
-function operatorClick(operator) {}
+function clearDisplay() {
+    display.innerText = "0";
+    num1 = null;
+    num2 = null;
+    operator = null;
+}
 
-function clearDisplay() {}
+function operatorClick(operator) {
+
+}
+
 
 function calculate() {}
 
 function updateDisplay(value) {
-    const display = document.getElementById("display");
     display.innerText = value;
 }
+
+function toggleSign() {}
 
